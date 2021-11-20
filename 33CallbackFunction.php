dfbcc7fcf049d300b0906c48c4c5e93345e16091
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2021-11-20 14:13:21 +0700

    Pelajaran 20 Nov 2021

---
+++ 33CallbackFunction.php
@@ -0,0 +1,21 @@
+<?php
+
+// - Callback Function
+// 1. Callback adalah sebuah mekanisme sebuah function memanggil function lainnya sesaui dengan yang diberikan di argument/parameter
+// 2. Hal ini sebenarnya sudah kita pelajari di materi Variabel Function dan Anonymous Function
+// 3. Namun di PHP ada cara lain untuk implementasi callback, yaitu dengan menggunakan tipe data "callable" yang kita tambakan didalam argument 
+// 4. Dan untuk memanggil callback function tsb, kita bisa menggunakan function "call_user_func(callable, arguments)"
+
+// - Kode: Callback Function
+function sayHello (string $name, callable $filter)
+{
+    $finalName = call_user_func($filter, $name);
+    echo "Hello $finalName" . PHP_EOL;
+}
+
+sayHello('Adrian', "strtoupper");
+sayHello('Adrian', "strtolower");
+sayHello("Manda", function (string $name): string {
+    return strtoupper($name);
+});
+sayHello('Acha', fn(string $name) => strtolower($name)); 
---
+++ 34RecursiveFunction.php
@@ -0,0 +1,49 @@
+<?php
+
+// - Recursive Function
+// 1. Recursive Function adalah kemampuan function untuk memanggil dirinya sendiri (memanggil function dirinya sendiri)
+// 2. Kadang memang ada banyak problem, yang lebih mudah diselesaikan menggunakan recursive, function, seperti contohnya kasus factorial
+
+// - Kode Factorial Menggunakan Loop
+function factorialLoop (int $value): int
+{
+    $total = 1;
+
+    for ($i=1; $i <= $value ; $i++) { 
+        $total *= $i;
+    }
+
+    return $total;
+}
+var_dump(factorialLoop(5));
+
+// - Kode Factorial Menggunakan Recursive Function
+function factorialRecursive (int $value): int
+{
+    if ($value == 1) {
+        return 1;
+    } else {
+        return $value * factorialRecursive($value - 1);
+        // 20 12 6 2 1
+    }
+}
+var_dump(factorialRecursive(5));
+
+// ================================================================================================
+
+// - Problem Recursive Function
+// 1. Walaupun recursive function itu sangat menarik, namun kita perlu berhati-hati
+// 2. Jika recursive terlalu dalam, maka akan ada kemungkinan terjadi memory overflow, yaitu error dimana memory terlalu banyak digunakan di PHP
+// 3. Kenapa problem ini terjadi? Karena kerika kita memanggil function, PHP akan menyimpannya didalam stack, jika function tsb memanggil function lain, maka stack akan menumpuk teus, dan jika terlalu banyak, makan akan membutuhkan konsumsi memory besar, jika sudah melewati batas, maka kan terjadi error memory
+
+// - Kode: Error StackOverflow
+function loop(int $value)
+{
+    if ($value == 0) {
+        echo "End Loop" . PHP_EOL;
+    } else {
+        echo "Loop-$value" . PHP_EOL;
+        loop($value - 1);
+    }
+}
+loop(3000000);
