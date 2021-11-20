d5b48cd02006bc8b8e44d3d8f1f895fbd8a8b436
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2021-11-20 15:42:34 +0700

    update materi function

--- 27Function.php
+++ 27Function.php
@@ -2,26 +2,25 @@
 
 // - Function
 // 1. Function adalah block kode program yang akan berjalan saat kita panggil
-// 2. Sebelumnya kita sudah menggunakan function isset(), count(), dan lain-lain
-// 3. Untuk membuat function di PHP, kita bisa menggunakan kata kunci funtion, lalu diikuti dengan nama function, kurung () dan diakhiri dengan block
-// 4. Kita bisa memanggil function dengan menggunakan nama function lalu diikuti dengan kurung "()"
+// 2. Sebelumnya kita sudah menggunakan function isset(), count(), dan lain-lain ini juga termasuk function (built in function dari php)
+// 3. Untuk membuat function di PHP, kita bisa menggunakan kata kunci funtion, lalu diikuti dengan nama function, kurung "()" dan diakhiri dengan block "{}"
+// 4. Kita bisa memanggil function dengan menggunakan nama function lalu diikuti dengan kurung "()" (nama_function())
 // 5. Dibahasa pemrograman lain, function juga disebut dengan method
 
 // - Syntax Function
 // function function_name(#parameter) {
 //     # code... / statement..
 // }
-
 // - Memanggil Function
 // nama_function();
 
 // - Contoh Function 1:
-// function sayHello() {
-//     echo "Hello Function" . PHP_EOL;
-// }
+function sayHello() {
+    echo "Hello Function" . PHP_EOL;
+}
 // Menjalankan function
-// sayHello();
-// sayHello();
+sayHello();
+sayHello();
 
 // - Lokasi Function
 // 1. PHP sangat fleksibel dalam pembuatan function
@@ -32,11 +31,11 @@
 // - Contoh Function Belum Dijalankan
 $buat = true;
 if ($buat) {
-    function sayHello() {
-        echo "Hello Function" . PHP_EOL;
+    function sayHay() {
+        echo "Hay Function" . PHP_EOL;
     }
 }
-sayHello();
+sayHay();
 // Jika dijalankan maka akan error kenapa? disini bisa dilihat variabel $buat bernilai "false"
 // Ketika didalam pengkondisian variabel tersebut bernilai salah / false
 // Dan ketika kita panggil functionnya maka akan error, karna function ini isitilahnya belum dipanggil / dijalankan / dibuat didalam php 
