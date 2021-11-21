e5b939d0be7619ec47a8deb5ca0bb4b1a422dbfa
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2021-11-21 13:51:43 +0700

    update materi recursive function

--- 34RecursiveFunction.php
+++ 34RecursiveFunction.php
@@ -29,6 +29,62 @@ function factorialRecursive (int $value): int
 }
 var_dump(factorialRecursive(5));
 
+// - Contoh Fungsi Rekursif 1: tampilkan data (echo), lalu lakukan proses rekursif
+// function tampilkanAngka(int $jumlah, int $indeks = 1) 
+// {
+//     echo "Perulangan ke-{$indeks}" . PHP_EOL;
+//     # Panggil dirinya sendiri selama $indeks < $jumlah
+//     if ($indeks < $jumlah) {
+//         tampilkanAngka($jumlah, $indeks + 1);
+//     }
+// }
+// tampilkanAngka(10);
+// Hasil:
+// Perulangan ke-1
+// Perulangan ke-2
+// Perulangan ke-3
+// Perulangan ke-4
+// Perulangan ke-5
+// Perulangan ke-6
+// Perulangan ke-7
+// Perulangan ke-8
+// Perulangan ke-9
+// Perulangan ke-10
+
+// - Contoh Fungsi Rekursif 2: Lakukan proses rekursif lalu menampilkan data (echo)
+function tampilkanAngka(int $jumlah, int $indeks = 1) 
+{
+    // Panggil dirinya sendiri selama $indeks < $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+
+    echo "Perulangan ke-{$indeks}" . PHP_EOL;
+
+}
+// Panggil function tampilAngka()
+tampilkanAngka(10);
+
+// Hasil:
+// Perulangan ke-10
+// Perulangan ke-9
+// Perulangan ke-8
+// Perulangan ke-7
+// Perulangan ke-6
+// Perulangan ke-5
+// Perulangan ke-4
+// Perulangan ke-3
+// Perulangan ke-2
+// Perulangan ke-1
+
+// Kenapa seperti itu beda dengan contoh yang pertama?
+// 1. Ketika fungsi tampilkanAngka dipanggil pertama kali, ia akan langsung memanggil dirinya sendiri.
+// 2. Ketika ia dipanggil untuk ke-2 kalinya, ia juga langsung memanggil dirinya sendiri.
+// 3. Ia terus melakukan hal yang sama sampai nilai $indeks = nilai $jumlah alias bernilai 20.
+// 4. Baru setelah rantai paling akhir tidak lagi memanggil dirinya sendiri, ia akan mulai menampilkan angka 20.
+// 5. Setelah rantai paling akhir selesai menampilkan angka 20, maka proses yang ke-19 pun dianggap telah selesai dari proses rekursif, dan ia akan mulai meng-echo angka 19.
+// 6. Begitu seterusnya hingga kembali ke perulangan yang pertama.
+
 // ================================================================================================
 
 // - Problem Recursive Function
