88f94579015dd8bf93c5c5a505f314c176b71bec
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-08 19:42:09 +0700

    update materi operator logika

--- 12OperatorLogika.php
+++ 12OperatorLogika.php
@@ -9,18 +9,21 @@
 // True jika $a dan $b keduanya true 
 var_dump(true && true);
 var_dump(true && false);
+echo PHP_EOL;
 
 // - OR (||)
 // $a || $b
 // True jika $a dan $b salah satu atau keduanya true
 var_dump(true || false);
 var_dump(true || true);
+echo PHP_EOL;
 
 // - NOT (!) 
 // !$a 
 // Dibalik datanya jika dia true jadi false, jika dia false jadi true
 // True jika $a bernilai false
 var_dump(!true);
+echo PHP_EOL;
 
 // - XOR (xor)
 // $a xor $b 
@@ -28,3 +31,4 @@ var_dump(!true);
 var_dump(true xor true);
 var_dump(true xor false);
 var_dump(false xor true);
+echo PHP_EOL;
