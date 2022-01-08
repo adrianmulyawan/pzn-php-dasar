711405407ca8e69a2b487983e33bb6435bd80dbf
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-08 09:40:02 +0700

    update materi tipe data boolean

--- 3TipeDataBoolean.php
+++ 3TipeDataBoolean.php
@@ -3,8 +3,8 @@
 // Tipe Data Boolean
 // 1. Tipe data paling sederhana di PHP 
 // 2. Tipe data dengan nilai kebenaran (Benar / Salah)
-// 3. Nilai benar direpresentasikan dengan: True
-// 4. Nilai salah direpresentasikan dengan: False
+// 3. Nilai benar direpresentasikan dengan: True (Case Insensitive)
+// 4. Nilai salah direpresentasikan dengan: False (Case Insensitive)
 
 echo "Benar: ";
 var_dump(true);
