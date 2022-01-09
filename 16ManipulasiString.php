4932a661dbfd47f3a82878e5aba2eb49b05e265d
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-09 14:21:13 +0700

    update materi manipulasi string

--- 16ManipulasiString.php
+++ 16ManipulasiString.php
@@ -4,7 +4,7 @@
 
 // 1. DOT Operator
 // > Dot (titik) operator adalah operator yang bisa kita gunakan untuk menambahkan string dengan data lain (bisa string atau data lainnya)
-// > Sebenarnnya kita bisa menggunakan + (plus) untuk menambahkan string, namun jika kita ocba tambahkan string dengan tipe data number, maka akan terjadi error. Oleh karena itu direkomendasikan menggunakan "." (dot) untuk menambahkan string dengan kata lain
+// > Sebenarnnya kita bisa menggunakan + (plus) untuk menambahkan string, namun jika kita coba tambahkan string dengan tipe data number, maka akan terjadi error. Oleh karena itu direkomendasikan menggunakan "." (dot) untuk menambahkan string dengan kata lain
 
 // Contoh DOT Operator
 $nama = "Adrian Mulyawan";
@@ -12,13 +12,16 @@ $nilai = 100;
 
 echo "Nama: " . $nama . PHP_EOL;
 echo "Nilai: " . $nilai . PHP_EOL . PHP_EOL;
+// Hasil: 
+// > Nama: Adrian Mulyawan
+// > Nilai: 100
 
-// ==============================================================================================
+// =============================================================================================
 
 // 2. Konversi ke Number dan Sebaliknya
 // > Kadang kita sering melakukan konversi tipe data dari string ke number (int/float) atau sebaliknya.
 // > Di PHP untuk konversi cukup mudah, kita hanya perlua menggunakan tanda kurung "(tipe_data)value".
-// > Tipe data untuk string adalah string, untuk number bisa menggunakan int untu integer dan float untuk floating point.
+// > Tipe data untuk string adalah string, untuk number bisa menggunakan int untuk integer dan float untuk floating point.
 
 // Contoh Konvervsi ke Number
 $valueString = (string)100;
@@ -36,7 +39,7 @@ var_dump($valueSalah);
 
 echo PHP_EOL;
 
-// ==============================================================================================
+// =============================================================================================
 
 // 3. Mengakses Karakter
 // > String di PHP seperti array karakter, dimana kita bisa mengakses data tiap karakternya menggunakan kurung siku "[]"
@@ -53,9 +56,9 @@ echo $makanan[3] . PHP_EOL . PHP_EOL;
 // ==============================================================================================
 
 // 4. Variabel Parsing
-// > Khusu string menggunakan Doublue Quote atau Heredoc, kita bisa menggunakan karakter $ untuk mengakses variabel
+// > Khusus string menggunakan Doublue Quote atau Heredoc, kita bisa menggunakan karakter "$" untuk mengakses variabel
 // > Ini memudahkan keika kita ingin menggabungkan string dengan variable
-// > Cara penggunaanya adalah menggunakan karakter $ lalu diikuti dengan nama variabelnya
+// > Cara penggunaanya adalah menggunakan karakter "$" lalu diikuti dengan nama variabelnya
 
 // Contoh Variable Parsing
 $contoh = "Adrian Mulyawan";
