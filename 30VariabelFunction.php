0bde6c1ef7ba2c34015d60400651221bd2a7831f
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-15 16:50:27 +0700

    update materi variable function

--- 30VariabelFunction.php
+++ 30VariabelFunction.php
@@ -8,13 +8,19 @@
 // - Contoh Variable Function
 // function foo() 
 // {
-//     echo "Foo" . PHP_EOL;
+    // echo "Foo" . PHP_EOL;
 // }
 
 // function bar()
 // {
-//     echo "Bar" . PHP_EOL;
+    // echo "Bar" . PHP_EOL;
 // }
+
+// Panggil Function
+// foo();
+// foo();
+// bar();
+// bar();
 
 // $functionFoo = "foo";
 // $functionFoo();
@@ -26,20 +32,55 @@
 // $functionBarbar = "barr";
 // $functionBarbar();
 
+// Variabel Function: Memanggil Function Yang Memerlukan Argument/Parameter
+// function sayHello(string $name)
+// {
+    // echo "Hello, $name" . PHP_EOL;
+// }
+
+// $functionName = "sayHello";
+// $functionName('Adrian');
+
 // ================================================================================================
 
-// - Kegunaan Variable Function
-// Kegunaan Variable Function ini adalah kita bisa menambahkan function sebagai argument/parameter dari "function" yang lain
+// - Kegunaan Variable Function 1
+// Kegunaan Variable Function ini adalah kita bisa membuat/menambah argument/parameter disebuah "function" yang dimana kita bisa memasukan "function" yang lain
+// Intinya kita bisa menambahkan function yang lain didalam parameter functionnya
 
 // - Contoh Variable Function 1
 // function sayHello(string $name, $filter) 
 // {
-//     $finalName = $filter($name);
-//     echo "Hello $finalName" . PHP_EOL;
+    // $finalName = $filter($name);
+    // echo "Hello $finalName" . PHP_EOL;
 // }
 
-// sayHello('wawan', 'strtoupper');
+// sayHello("wawan", "strtoupper");
 // Hasil: WAWAN
+// Penjelasan:
+// 1. Disini kita mempunyai function dengan nama sayHello(), dimana function tsb memiliki 2 parameter:
+// > string $nama > $filter
+// 2. $filter ini adalah parameter yang berupa function lain yang akan dipanggil
+// 3. Seblum kita jalankan display / echo ("Hello $finalName" . PHP_EOL;) kita akan konversi parameter namanya (string $nama) dengan parameter filter ($filter)
+// 4. Lalu kita simpan nilainya kedalam variable $finalName dan kemudian kita tampilkan dengan echo
+// 5. Kemudian disini kita jalankan function sayHello() nya dengan mengirimkan 2 parameter
+// > string $name = wawan
+// > $filter = strtoupper (function bawaan dari php untuk membuat teks besar semua)
+// 6. Dan yang terjadi didalam functionnya akan seperti ini
+// 6.1 Isi dari function sayHello()
+// function sayHello("wawan", "strtoupper") 
+// {
+    // $finalName = strtoupper(wawan);
+    // echo "Hello $finalName" . PHP_EOL;
+// }
+// 7. Sehingga hasil echo = Hello WAWAN
+
+// - Atau bisa dibuat seperti ini:
+// $functionHello = "sayHello";
+// $functionHello("wawan", "strtoupper");
+
+// ================================================================================================
+
+// - Kegunaan Variable Function 2
 
 // - Contoh Variable Function 2: dengan menambahkan function buatan sendiri
 function sayHello(string $name, $filter) 
@@ -55,5 +96,8 @@ function sampleFunction(string $name): string
 
 sayHello('Wawan', "sampleFunction");
 // Hasil: Hello Sample Wawan
+
 sayHello('wawan', 'strtoupper');
 sayHello('MANDA', 'strtolower');
+
+// Dengan variable function kita seakan-akan bisa mengirimkan function lain kedalam argument/parameter function yang kita miliki
