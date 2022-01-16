331b281752ec0ed5d31df85f667751572b08a3b4
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-16 11:51:47 +0700

    update materi Arrow Function

--- 32ArrowFunction.php
+++ 32ArrowFunction.php
@@ -4,7 +4,7 @@
 // 1. Arrow Function diperkenalkan di PHP 7.4 sebagai alternative anonymous function yang lebih sederhana cara pembuatannya
 // 2. Secara garis besar, sebenarnya arrow function dan anonymous function adalah 2 hal yang sama
 // 3. Hal yang membedakan di arrow function adalah,secara otomatis variable diluar closure bisa digunakan, tidak seperti di anonymous function yang harus disebutkan secara manual menggunakan kata kunci "use ($namaVariable)"
-// 4. Pembuatan arrow function tidak menggunakan kata kunci "function" melainkan "fn()"
+// 4. Pembuatan arrow function tidak menggunakan kata kunci "function" melainkan "fn("$parameter")"
 // 5. Arrow function dikhususkan untuk pembuatan function yang sederhana
 
 // - Kode: Pembuatan Arrow Function
